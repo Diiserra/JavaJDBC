8fc00357ff193186e3ebc1a1fadb0b31b9e29853
Diego Lima <diiserras@gmail.com>
2021-07-15 22:52:24 -0300

    Using Statement Class and ResulSet Class

--- src/dbServices/DB.java
+++ src/dbServices/DB.java
@@ -2,9 +2,7 @@ package dbServices;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -38,6 +36,26 @@ public class DB {
         if (conn != null){
             try {
                 conn.close();
+            } catch (SQLException e) {
+                throw new DbExeception(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if (st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbExeception(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
             } catch (SQLException e) {
                 throw new DbExeception(e.getMessage());
             }
